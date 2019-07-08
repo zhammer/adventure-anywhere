dfa6b3b9ef0cdf102874dbf18335fac45b631fe5
zhammer <zachary_hammer@alumni.brown.edu>
2019-07-08 01:58:27 -0400

    Add support for help stop cancel and session ended requests

--- alexa_adventure/handler.py
+++ alexa_adventure/handler.py
@@ -5,16 +5,52 @@ from adventure_anywhere.definitions import Config, Context
 from adventure_anywhere.exceptions import CommandPolicyViolationError
 from adventure_anywhere.s3_bucket_saves import S3BucketSavesGateway
 from adventure_anywhere.use_adventure import do_command, last_output
+from adventure_anywhere.game import GameEngine
+
+
+session_close_prompt = (
+    "SAVING PROGRESS IN ADVENTURE. RESUME GAME BY SAYING 'ALEXA, OPEN ADVENTURE.'"
+)
 
 
 def handle(event: Dict, context: Any) -> Dict:
     request_type = event["request"]["type"]
+    intent_name = event["request"].get("intent", {}).get("name", "")
     if request_type == "LaunchRequest":
         return _handle_launch_request(event, context)
-    elif request_type == "IntentRequest":
-        return _handle_intent_request(event, context)
+    elif request_type == "HelpIntent":
+        return _handle_help_request(event, context)
+    elif request_type == "IntentRequest" and intent_name == "CommandIntent":
+        return _handle_command_intent_request(event, context)
+    elif request_type == "IntentRequest" and intent_name == "AMAZON.HelpIntent":
+        return _handle_help_request(event, context)
+    elif request_type == "IntentRequest" and intent_name in (
+        "AMAZON.CancelIntent",
+        "AMAZON.StopIntent",
+    ):
+        return _handle_stop_request(event, context)
+    elif request_type == "SessionEndedRequest":
+        return _handle_session_ended_request(event, context)
     else:
         raise RuntimeError(f"Cannot handle {request_type}")
+
+
+def _handle_help_request(event: Dict, context: Any) -> Dict:
+    return _build_response(GameEngine.help_prompt(), should_session_end=False)
+
+
+def _handle_stop_request(event: Dict, context: Any) -> Dict:
+    return _build_response(session_close_prompt, should_session_end=True)
+
+
+def _handle_session_ended_request(event: Dict, context: Any) -> Dict:
+    reason = event["request"]["reason"]
+    out = (
+        f"AN ERROR OCCURED. {session_close_prompt}"
+        if reason == "ERROR"
+        else session_close_prompt
+    )
+    return _build_response(out, should_session_end=True)
 
 
 def _handle_launch_request(event: Dict, context: Any) -> Dict:
@@ -27,10 +63,10 @@ def _handle_launch_request(event: Dict, context: Any) -> Dict:
 
     out = last_output(context, player_id=user_id)
 
-    return _build_response(out)
+    return _build_response(out, should_session_end=False)
 
 
-def _handle_intent_request(event: Dict, context: Any) -> Dict:
+def _handle_command_intent_request(event: Dict, context: Any) -> Dict:
     bucket_name = os.environ["SAVES_S3_BUCKET"]
 
     user_id = event["session"]["user"]["userId"]
@@ -45,15 +81,15 @@ def _handle_intent_request(event: Dict, context: Any) -> Dict:
     except CommandPolicyViolationError as e:
         out = str(e)
 
-    return _build_response(out)
+    return _build_response(out, should_session_end=False)
 
 
-def _build_response(response_text: str) -> Dict:
+def _build_response(response_text: str, *, should_session_end: bool) -> Dict:
     return {
         "version": "1.0",
         "response": {
             "outputSpeech": {"text": response_text, "type": "PlainText"},
-            "shouldEndSession": False,
+            "shouldEndSession": should_session_end,
         },
     }
 
