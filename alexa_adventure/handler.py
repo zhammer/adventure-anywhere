b690750ab341da8c7feaecd64e5afe2e9cfff01a
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-07-11 21:05:19 -0400

    Last alexa functional fixes (#4)
    * Add Skill helper class for alexa request and intent handlers
    
    * Update invocationName to "cave adventure"
    
    From certification feedback:
    > One word invocation names are not allowed, unless the invocation
    name is unique to your brand/intellectual property.
    
    * Add _replace_type_with_say output processor
    
    From certificate feedback:
    > Please note that in the above response skill is asking users to TYPE
    > "RESTART" TO RESTART THE GAME. In alexa device there is no option to
    > type, consider removing that statement.
    > Also note that same behaviour is observed while in navigation of the
    > skill.
    
    * Improve restart notice wording
    
    * Add _replace_type_with_say postprocess to launch request output

--- alexa_adventure/handler.py
+++ alexa_adventure/handler.py
@@ -1,58 +1,50 @@
 import os
+import re
 from typing import Any, Dict, Optional, cast
 
 from adventure_anywhere.definitions import Config, Context
 from adventure_anywhere.exceptions import CommandPolicyViolationError
+from adventure_anywhere.game import GameEngine
 from adventure_anywhere.s3_bucket_saves import S3BucketSavesGateway
 from adventure_anywhere.use_adventure import do_command, last_output
-from adventure_anywhere.game import GameEngine
-
+from alexa_adventure.util import Skill
 
 session_close_prompt = (
-    "SAVING PROGRESS IN ADVENTURE. RESUME GAME BY SAYING 'ALEXA, OPEN ADVENTURE.'"
+    "SAVING PROGRESS IN ADVENTURE. RESUME GAME BY SAYING 'ALEXA, OPEN CAVE ADVENTURE.'"
 )
 
 
-def handle(event: Dict, context: Any) -> Dict:
-    request_type = event["request"]["type"]
-    intent_name = event["request"].get("intent", {}).get("name", "")
-    if request_type == "LaunchRequest":
-        return _handle_launch_request(event, context)
-    elif request_type == "HelpIntent":
-        return _handle_help_request(event, context)
-    elif request_type == "IntentRequest" and intent_name == "CommandIntent":
-        return _handle_command_intent_request(event, context)
-    elif request_type == "IntentRequest" and intent_name == "AMAZON.HelpIntent":
-        return _handle_help_request(event, context)
-    elif request_type == "IntentRequest" and intent_name in (
-        "AMAZON.CancelIntent",
-        "AMAZON.StopIntent",
-    ):
-        return _handle_stop_request(event, context)
-    elif request_type == "SessionEndedRequest":
-        return _handle_session_ended_request(event, context)
-    else:
-        raise RuntimeError(f"Cannot handle {request_type}")
+skill = Skill()
+handle = skill.handle
 
 
+@skill.intent("AMAZON.HelpIntent")
 def _handle_help_request(event: Dict, context: Any) -> Dict:
-    return _build_response(GameEngine.help_prompt(), should_session_end=False)
+    out = _replace_type_with_say(GameEngine.help_prompt())
+    return _build_response(out, should_session_end=False)
 
 
+@skill.intent("AMAZON.CancelIntent")
+@skill.intent("AMAZON.StopIntent")
 def _handle_stop_request(event: Dict, context: Any) -> Dict:
     return _build_response(session_close_prompt, should_session_end=True)
 
 
+@skill.request("SessionEndedRequest")
 def _handle_session_ended_request(event: Dict, context: Any) -> Dict:
+    """
+    >>> _handle_session_ended_request({"request":{"reason": "ERROR"}}, None)["response"]["outputSpeech"]["text"] #doctest: +ELLIPSIS
+    "AN ERROR OCCURED. SAVING PROGRESS...ADVENTURE.'"
+    >>> _handle_session_ended_request({"request":{"reason": "USER_INITIATED"}}, None)["response"]["outputSpeech"]["text"] #doctest: +ELLIPSIS
+    "SAVING PROGRESS...ADVENTURE.'"
+    """
     reason = event["request"]["reason"]
-    out = (
-        f"AN ERROR OCCURED. {session_close_prompt}"
-        if reason == "ERROR"
-        else session_close_prompt
-    )
+    prefix = "AN ERROR OCCURED. " if reason == "ERROR" else ""
+    out = f"{prefix}{session_close_prompt}"
     return _build_response(out, should_session_end=True)
 
 
+@skill.request("LaunchRequest")
 def _handle_launch_request(event: Dict, context: Any) -> Dict:
     bucket_name = os.environ["SAVES_S3_BUCKET"]
 
@@ -61,11 +53,12 @@ def _handle_launch_request(event: Dict, context: Any) -> Dict:
     s3_bucket_saves = S3BucketSavesGateway(bucket_name)
     context = Context(saves=s3_bucket_saves)
 
-    out = last_output(context, player_id=user_id)
+    out = _replace_type_with_say(last_output(context, player_id=user_id))
 
     return _build_response(out, should_session_end=False)
 
 
+@skill.intent("CommandIntent")
 def _handle_command_intent_request(event: Dict, context: Any) -> Dict:
     bucket_name = os.environ["SAVES_S3_BUCKET"]
 
@@ -81,7 +74,7 @@ def _handle_command_intent_request(event: Dict, context: Any) -> Dict:
     except CommandPolicyViolationError as e:
         out = str(e)
 
-    return _build_response(out, should_session_end=False)
+    return _build_response(_replace_type_with_say(out), should_session_end=False)
 
 
 def _build_response(response_text: str, *, should_session_end: bool) -> Dict:
@@ -101,3 +94,21 @@ def _pluck_command(event: Dict) -> str:
     command_b: Optional[str] = slots.get("commandB", {}).get("value", None)
 
     return f"{command_a} {command_b}" if command_b else command_a
+
+
+def _replace_type_with_say(text: str) -> str:
+    """
+    >>> _replace_type_with_say("TYPED")
+    'TYPED'
+
+    >>> _replace_type_with_say("TYPE")
+    'SAY'
+
+    >>> _replace_type_with_say("TYPE W RATHER THAN WEST")
+    'SAY W RATHER THAN WEST'
+
+    >>> _replace_type_with_say('WELCOME TO ADVENTURE!! WOULD YOU LIKE INSTRUCTIONS? - - - (TYPE "RESTART" TO RESTART THE GAME.)')  #doctest: +ELLIPSIS
+    'WELCOME...SAY "RESTART"...'
+    """
+    return re.sub(r"\bTYPE\b", "SAY", text, flags=re.IGNORECASE)
+
